559257882c2c45d92b51fe1d633aba4ac6b783f9
LuzMieres <mieresluz5@gmail.com>
2024-08-09 13:29:25 -0300

    Sprint 1 completo

--- src/main/java/com/MindHub/Homebanking/HomebankingApplication.java
+++ src/main/java/com/MindHub/Homebanking/HomebankingApplication.java
@@ -1,7 +1,11 @@
 package com.MindHub.Homebanking;
 
+import com.MindHub.Homebanking.models.Client;
+import com.MindHub.Homebanking.repositories.ClientRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class HomebankingApplication {
@@ -9,5 +13,26 @@ public class HomebankingApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(HomebankingApplication.class, args);
 	}
+	@Bean
+	public CommandLineRunner initData (ClientRepository clientRepository){
+		return args -> {
+			//Creo y guardo al client Melba
+			Client melba = new Client("Melba", "Morel", "melba@mindhub.com");
+			clientRepository.save(melba);
 
+			//Creo y guardo al client Ana
+			Client ana = new Client("Ana", "Gonzalez", "anaGonzalez@gmail.com");
+			clientRepository.save(ana);
+
+			//Creo y guardo al client Luz
+			Client luz = new Client("Luz", "Mieres", "luzmieres@gmail.com");
+			clientRepository.save(luz);
+
+			//Souts de los 3 Clientes
+			System.out.println(melba);
+			System.out.println(ana);
+			System.out.println(luz);
+
+		};
+	}
 }
---
+++ src/main/java/com/MindHub/Homebanking/repositories/ClientRepository.java
@@ -0,0 +1,9 @@
+package com.MindHub.Homebanking.repositories;
+
+import com.MindHub.Homebanking.models.Client;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ClientRepository extends JpaRepository<Client, Long> {
+}
